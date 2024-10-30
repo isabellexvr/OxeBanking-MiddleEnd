c7b9987e195755fff050a27d1448f0dfe767f6e3
isabellexvr <isabellexavier59@gmail.com>
2024-10-30 08:17:50 -0300

    feat: mocked controllers

--- src/controllers/payments_controller.rs
+++ src/controllers/payments_controller.rs
@@ -1,21 +1,141 @@
-use actix_web::{post, web, HttpRequest, HttpResponse, Responder};
+use actix_web::{post, get, web, HttpRequest, HttpResponse, Responder};
 use crate::dto::payment_dto::PaymentDTO;
 use crate::dto::jwt::Claims;
-use crate::microservices::payments::{post_new_payment};
+use crate::models::payment::Payment;
+use crate::microservices::payments::{post_new_payment, get_payment_by_id};
 use actix_web::HttpMessage;
 
 #[post("/new")]
 async fn create_payment(payment: web::Json<PaymentDTO>, req: HttpRequest) -> impl Responder {
     // Verifica se as informações do token (Claims) foram armazenadas no request pelo middleware
-    if let Some(claims) = req.extensions().get::<Claims>() {
+/*     if let Some(claims) = req.extensions().get::<Claims>() {
         println!("User ID from token: {}", claims.user_id);
         println!("User name from token: {}", claims.sub);
 
-    }
+    } */
 
     // Chama o microserviço de pagamentos para criar um novo pagamento
     let response = post_new_payment(payment).await.unwrap();
     println!("Response from microservice: {}", response);
 
     HttpResponse::Ok().json("Payment created!")
+}
+
+#[get("/all/{user_id}")]
+async fn get_payments_by_user_id(user_id: web::Path<i32>) -> impl Responder {
+    let mocked_payments = vec![
+        Payment {
+            id: 1,
+            descricao: "Pagamento de aluguel".to_string(),
+            id_usuario: *user_id,
+            metodo_pagamento: "credito".to_string(),
+            valor: 100000,
+            saldo: 500000,
+            fatura_pendente: None,
+            destinatario: 2,
+            inserted_at: "2021-08-01T00:00:00Z".to_string(),
+            updated_at: "2021-08-01T00:00:00Z".to_string(),
+        },
+        Payment {
+            id: 2,
+            descricao: "Pagamento de condomínio".to_string(),
+            id_usuario: *user_id,
+            metodo_pagamento: "credito".to_string(),
+            valor: 50000,
+            saldo: 450000,
+            fatura_pendente: None,
+            destinatario: 3,
+            inserted_at: "2021-08-01T00:00:00Z".to_string(),
+            updated_at: "2021-08-01T00:00:00Z".to_string(),
+        },
+        Payment {
+            id: 3,
+            descricao: "Pagamento de luz".to_string(),
+            id_usuario: *user_id,
+            metodo_pagamento: "pix".to_string(),
+            valor: 20000,
+            saldo: 430000,
+            fatura_pendente: None,
+            destinatario: 4,
+            inserted_at: "2021-08-01T00:00:00Z".to_string(),
+            updated_at: "2021-08-01T00:00:00Z".to_string(),
+        },
+        Payment {
+            id: 4,
+            descricao: "Pagamento de internet".to_string(),
+            id_usuario: *user_id,
+            metodo_pagamento: "credito".to_string(),
+            valor: 15000,
+            saldo: 415000,
+            fatura_pendente: None,
+            destinatario: 5,
+            inserted_at: "2021-08-01T00:00:00Z".to_string(),
+            updated_at: "2021-08-01T00:00:00Z".to_string(),
+        },
+        Payment {
+            id: 5,
+            descricao: "Pagamento de academia".to_string(),
+            id_usuario: *user_id,
+            metodo_pagamento: "credito".to_string(),
+            valor: 30000,
+            saldo: 385000,
+            fatura_pendente: Some(2500),
+            destinatario: 6,
+            inserted_at: "2021-08-01T00:00:00Z".to_string(),
+            updated_at: "2021-08-01T00:00:00Z".to_string(),
+        }
+    ];
+
+    HttpResponse::Ok().json(mocked_payments)
+}
+
+#[get("/latest/{user_id}")]	
+async fn get_latest_payments_by_user_id(user_id: web::Path<i32>) -> impl Responder {
+    let mocked_payments = vec![
+        Payment {
+            id: 1,
+            descricao: "Pagamento de aluguel".to_string(),
+            id_usuario: *user_id,
+            metodo_pagamento: "credito".to_string(),
+            valor: 100000,
+            saldo: 500000,
+            fatura_pendente: None,
+            destinatario: 2,
+            inserted_at: "2021-08-01T00:00:00Z".to_string(),
+            updated_at: "2021-08-01T00:00:00Z".to_string(),
+        },
+        Payment {
+            id: 2,
+            descricao: "Pagamento de condomínio".to_string(),
+            id_usuario: *user_id,
+            metodo_pagamento: "credito".to_string(),
+            valor: 50000,
+            saldo: 450000,
+            fatura_pendente: None,
+            destinatario: 3,
+            inserted_at: "2021-08-01T00:00:00Z".to_string(),
+            updated_at: "2021-08-01T00:00:00Z".to_string(),
+        },
+        Payment {
+            id: 3,
+            descricao: "Pagamento de luz".to_string(),
+            id_usuario: *user_id,
+            metodo_pagamento: "pix".to_string(),
+            valor: 20000,
+            saldo: 430000,
+            fatura_pendente: None,
+            destinatario: 4,
+            inserted_at: "2021-08-01T00:00:00Z".to_string(),
+            updated_at: "2021-08-01T00:00:00Z".to_string(),
+        },
+    ];
+
+    HttpResponse::Ok().json(mocked_payments)
+}
+
+#[get("/details/{payment_id}")]
+async fn get_payment_info(payment_id: web::Path<i32>) -> impl Responder {
+    let response = get_payment_by_id(*payment_id).await.unwrap();
+
+    HttpResponse::Ok().json(response)
 }
--- src/models/payment.rs
+++ src/models/payment.rs
@@ -1,13 +1,28 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PaymentBD {
+    pub id: i32,
+    pub descricao: String,
+    pub id_usuario: i32,
+    pub metodo_pagamento: String,
+    pub valor: String,
+    pub saldo: String,
+    pub fatura_pendente: Option<String>,
+    pub destinatario: i32,
+    pub inserted_at: String,
+    pub updated_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Payment {
     pub id: i32,
     pub descricao: String,
     pub id_usuario: i32,
     pub metodo_pagamento: String,
-    pub valor: f64,
-    pub saldo: f64,
+    pub valor: i32,
+    pub saldo: i32,
+    pub fatura_pendente: Option<i32>,
     pub destinatario: i32,
     pub inserted_at: String,
     pub updated_at: String,
