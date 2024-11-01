2016a30d99c43be388ef3ac892f2e839a646e2fa
TheMarksan <marcosmelodossantos.com@gmail.com>
2024-11-01 14:42:24 -0300

    feat: database_sqlite

--- migrations/20241101170736_initial.sql
+++ migrations/20241101170736_initial.sql
@@ -1,23 +1,32 @@
-CREATE TABLE accounts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+-- Add migration script here
+CREATE TABLE account_bd (
+    id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     balance INTEGER NOT NULL,
     gross_mensal_income INTEGER NOT NULL
 );
 
-CREATE TABLE br_accounts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE account_pix_keys_bd (
+    id INTEGER PRIMARY KEY,
+    account_id INTEGER NOT NULL,
+    pix_key TEXT NOT NULL,
+    key_type TEXT NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account_bd(id)
+);
+
+CREATE TABLE br_account_bd (
+    id INTEGER PRIMARY KEY,
     account_id INTEGER NOT NULL,
     account_number TEXT NOT NULL,
     agency TEXT NOT NULL,
     account_type TEXT NOT NULL,
     bank_name TEXT NOT NULL,
     bank_code TEXT NOT NULL,
-    FOREIGN KEY(account_id) REFERENCES accounts(id)
+    FOREIGN KEY (account_id) REFERENCES account_bd(id)
 );
 
-CREATE TABLE global_accounts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE global_account_bd (
+    id INTEGER PRIMARY KEY,
     account_id INTEGER NOT NULL,
     account_number TEXT NOT NULL,
     ach_routing_number TEXT NOT NULL,
@@ -25,13 +34,5 @@ CREATE TABLE global_accounts (
     bank_name TEXT NOT NULL,
     bank_code TEXT NOT NULL,
     bank_address TEXT NOT NULL,
-    FOREIGN KEY(account_id) REFERENCES accounts(id)
-);
-
-CREATE TABLE account_pix_keys (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    account_id INTEGER NOT NULL,
-    pix_key TEXT NOT NULL,
-    key_type TEXT NOT NULL,
-    FOREIGN KEY(account_id) REFERENCES accounts(id)
+    FOREIGN KEY (account_id) REFERENCES account_bd(id)
 );
